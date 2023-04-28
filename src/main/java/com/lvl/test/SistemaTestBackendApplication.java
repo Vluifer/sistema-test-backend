de3a65ce083b084a5cd70b06afe4d97af0140a12
Luifer Villa <villaluifer@gmail.com>
2023-04-27 21:19:52 -0500

    first commit

---
+++ src/main/java/com/lvl/test/SistemaTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lvl.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lvl/test/SistemaTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvl.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
