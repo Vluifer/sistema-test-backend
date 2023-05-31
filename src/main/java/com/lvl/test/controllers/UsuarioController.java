0ccc7fdc6ac1bfff92abad18f94afd0f70ed921e
Luifer Villa <villaluifer@gmail.com>
2023-05-30 21:49:09 -0500

    Cración de controlador

--- src/main/java/com/lvl/test/controllers/UsuarioController.java
+++ src/main/java/com/lvl/test/controllers/UsuarioController.java
@@ -1,10 +1,46 @@
 package com.lvl.test.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lvl.test.entidades.Rol;
+import com.lvl.test.entidades.Usuario;
+import com.lvl.test.entidades.UsuarioRol;
+import com.lvl.test.servicios.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/usuarios")
 
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public Usuario guardarUsuario (@RequestBody Usuario usuario) throws Exception {
+        Set<UsuarioRol> roles = new HashSet<>();
+
+        Rol rol =new Rol();
+        rol.setRolId(2L);
+        rol.setNombre("NORMAL");
+
+        UsuarioRol usuarioRol =new UsuarioRol();
+        usuarioRol.setUsuario(usuario);
+        usuarioRol.setRol(rol);
+
+        return usuarioService.guardarUsuario(usuario,roles);
+    }
+
+    @GetMapping("/{username}")
+    public Usuario obtenerUsuario(@PathVariable("username")String username){
+        return  usuarioService.obtenerUsuario(username);
+    }
+
+    @DeleteMapping("/{usuarioId}")
+    public void eliminarUsuario(@PathVariable("usuarioId")Long usuarioId){
+        usuarioService.eliminarUsuario(usuarioId);
+    }
+
 }
