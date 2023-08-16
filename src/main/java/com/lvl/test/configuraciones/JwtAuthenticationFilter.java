32efeb35eb497d2aee8f1c212ff33454bbab6604
Luifer Villa <villaluifer@gmail.com>
2023-08-16 18:01:35 -0500

    implementando JwtUtils

--- src/main/java/com/lvl/test/configuraciones/JwtAuthenticationFilter.java
+++ src/main/java/com/lvl/test/configuraciones/JwtAuthenticationFilter.java
@@ -1,20 +1,39 @@
 package com.lvl.test.configuraciones;
 
 import com.lvl.test.servicios.impl.UserDetailsServiceImpl;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
-    UserDetailsServiceImpl userDetailsService;
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestTokenHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwtToken = null;
 
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")){
+            jwtToken = requestTokenHeader.substring(7);
+
+            try {
+                username = this.jwtUtil.extractUsername(jwtToken);
+            }catch (ExpiredJwtException expiredJwtException){
+                System.out.println("El token hya expirado");
+            }catch (Exception exception){
+                exception.printStackTrace();
+            }
+        }
     }
 }
