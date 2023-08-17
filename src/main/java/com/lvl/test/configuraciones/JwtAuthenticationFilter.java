3cedb912f036ef34037ea937d5cce1b613295128
Luifer Villa <villaluifer@gmail.com>
2023-08-16 21:36:46 -0500

    Clase jwtFilter completa

--- src/main/java/com/lvl/test/configuraciones/JwtAuthenticationFilter.java
+++ src/main/java/com/lvl/test/configuraciones/JwtAuthenticationFilter.java
@@ -7,6 +7,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -34,6 +38,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }catch (Exception exception){
                 exception.printStackTrace();
             }
+        }else{
+            System.out.println("token invalido, no empieza con bearer string");
         }
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails  = this.userDetailsService.loadUserByUsername(username);
+            if (this.jwtUtil.validateToken(jwtToken,userDetails)){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }else {
+            System.out.println("El token no es valido");
+        }
+        filterChain.doFilter(request,response);
+
     }
 }
